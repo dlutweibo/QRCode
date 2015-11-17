e60c6300966806dc61da200f6b9796727cec20de
魏博 <weibo03@meituan.com>
2015-11-17 17:48:18 +0800

    Initial Commit

---
+++ QRCode/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  QRCode
+//
+//  Created by 魏博 on 15/11/17.
+//  Copyright © 2015年 魏博. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
